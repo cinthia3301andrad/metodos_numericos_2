3c238405307f790d2f22e5e14f0bb2dee4b8cb08
Danielecan <dannygoncalves41@gmail.com>
2021-06-21 23:13:41 -0300

    feat: Correção de Newton Cotes

--- trabalho1/newtonCotes.py
+++ trabalho1/newtonCotes.py
@@ -14,66 +14,72 @@ def funcao_geral_integracao(a, b, epson, tipo):
     Xi = 0
     erro = 0
     resultadoAnterior = 0
+    resultado = 0
     interacoes = 0
-    N = 1
+    N = 2
 
     while True:
+        interacoes += 1
         delta = (b-a)/N
-        resultado = 0
-        for i in range(0, int(N)):
+        integral = 0
+        for i in range(N):
             Xi = a + i*delta
             Xf = Xi + delta
             if(tipo=="fechada1"):
-                resultado += formula_1_fechada(delta, Xi)
+                integral += formula_1_fechada(Xi, Xf)
             elif(tipo=="fechada2"):
-                h = (b-a)/2
-                resultado += formula_2_fechada(h, Xi)
+                integral += formula_2_fechada(Xi, Xf)
             elif(tipo=="fechada3"):
-                h = (b-a)/3
-                resultado += formula_3_fechada(h, Xi)
+                integral += formula_3_fechada(Xi, Xf)
             elif(tipo=="fechada4"):
-                h = (b-a)/4
-                resultado += formula_4_fechada(h, Xi)
+                integral += formula_4_fechada(Xi, Xf)
             elif(tipo=="aberta1"):
-                h = (b-a)/3
-                resultado += formula_1_aberta(delta, h , Xi)
+                integral += formula_1_aberta(Xi, Xf)
             elif(tipo=="aberta2"):
-                h = (b-a)/4
-                resultado += formula_2_aberta(h, Xi)
+                integral += formula_2_aberta(Xi, Xf)
             elif(tipo=="aberta3"):
-                h = (b-a)/5
-                resultado += formula_3_aberta(h, Xi)
+                integral += formula_3_aberta(Xi, Xf)
             elif(tipo=="aberta4"):
-                h = (b-a)/6
-                resultado += formula_4_aberta(h, Xi)
-        erro = abs((resultado - resultadoAnterior)/ resultado)
-        resultadoAnterior = resultado
-        interacoes += 1
+                integral += formula_4_aberta(Xi, Xf)
         N = N*2
-        if (erro > epson): break
+        resultadoAnterior = resultado
+        resultado = integral
+        erro = abs(resultadoAnterior - resultado)
+        print(resultado, resultadoAnterior, erro)
+        
+        if (erro < epson): 
+            print('a')
+            break
     
     return interacoes, resultado
 
-def formula_1_fechada(delta, Xi): #GRAU 1
-    return (delta)/2*(funcaoIntegrada(Xi)+funcaoIntegrada(Xi+delta))
+def formula_1_fechada(Xi, Xf): #GRAU 1
+    return (Xf-Xi)/2*(funcaoIntegrada(Xi)+funcaoIntegrada(Xf))
     
-def formula_2_fechada(h, Xi): #GRAU 2
+def formula_2_fechada(Xi, Xf): #GRAU 2
+    h = (Xf-Xi)/2
     return (h)/3*(funcaoIntegrada(Xi) + 4*funcaoIntegrada(Xi+h) + funcaoIntegrada(Xi+2*h))
 
-def formula_3_fechada(h, Xi): #GRAU 3
+def formula_3_fechada(Xi, Xf): #GRAU 3
+    h = (Xf-Xi)/3
     return (3*(h)/8)*(funcaoIntegrada(Xi) + 3*funcaoIntegrada(Xi+h) + 3*funcaoIntegrada(Xi+2*h) +funcaoIntegrada(Xi+3*h))
 
-def formula_4_fechada(h, Xi): #GRAU 4    
+def formula_4_fechada(Xi, Xf): #GRAU 4 
+    h = (Xf-Xi)/4
     return (2*(h)/45)*(7*funcaoIntegrada(Xi) + 32*funcaoIntegrada(Xi+h) + 12*funcaoIntegrada(Xi+2*h) +32*funcaoIntegrada(Xi+3*h)+7*funcaoIntegrada(Xi+4*h))
         
-def formula_1_aberta(delta, h , Xi): #GRAU 1
-    return (delta)/2*(funcaoIntegrada(Xi+h)+funcaoIntegrada(Xi+2*h))
+def formula_1_aberta(Xi, Xf): #GRAU 1
+    h = (Xf-Xi)/3
+    return (Xf-Xi)/2*(funcaoIntegrada(Xi+h)+funcaoIntegrada(Xi+2*h))
 
-def formula_2_aberta(h, Xi): #GRAU 2
+def formula_2_aberta(Xi, Xf): #GRAU 2
+    h = (Xf-Xi)/4
     return ((4*h)/3)*(2*funcaoIntegrada(Xi+h) - funcaoIntegrada(Xi+2*h) + 2*funcaoIntegrada(Xi+3*h))   
 
-def formula_3_aberta(h, Xi): #GRAU 3
+def formula_3_aberta(Xi, Xf): #GRAU 3
+    h = (Xf-Xi)/5
     return ((5*h)/24)*(11*funcaoIntegrada(Xi+h) + funcaoIntegrada(Xi+2*h) + funcaoIntegrada(Xi+3*h) + 11*funcaoIntegrada(Xi+4*h))
 
-def formula_4_aberta(h, Xi): #GRAU 4
+def formula_4_aberta(Xi, Xf): #GRAU 4
+    h = (Xf-Xi)/6
     return ((6*h)/20)*(11*funcaoIntegrada(Xi+h) - 14*funcaoIntegrada(Xi+2*h) + 26*funcaoIntegrada(Xi+3*h) - 14*funcaoIntegrada(Xi+4*h)+ 11*funcaoIntegrada(Xi+5*h))
